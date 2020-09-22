9c8a2f723f61936dbb394d6edecd3eec8f7b1696
Marius Jakobi <marius.jakobi@googlemail.com>
2020-09-22 12:01:15 +0200

    Clean up

--- tests/Person.spec.ts
+++ tests/Person.spec.ts
@@ -5,14 +5,6 @@ import "mocha";
 describe("Person class", () => {
   const person = new Person("Jon", "Doe");
 
-  it("should return first name", () => {
-    expect(person.getFirstName()).to.equal("Jon");
-  });
-
-  it("should return full name", () => {
-    expect(person.getLastName()).to.equal("Doe");
-  });
-
   it("should return full name", () => {
     expect(person.getFullName()).to.equal("Jon Doe");
   });
